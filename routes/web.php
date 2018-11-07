6ca98b377f65cf5e03a30717f5fcd05b3f7a9116
Dung Duong <dtdung2911@gmail.com>
2018-11-07 09:09:29 +0700

    Laravel authentication

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['middleware' => 'locale'], function() {
-    Route::get('/', function () {
-        return view('welcome');
-    })->name('home');
+Route::group(['middleware' => 'locale'], function () {
 
     // Hiển thị danh sách bài viết
     Route::get('/posts', 'PostController@index')->name('posts.list');
 
-    // Hiển thị giao diện thêm mới bài viết
-    Route::get('/posts/create', 'PostController@create')->name('posts.create');
-
-    // Tạo mới bài viết
-    Route::post('/posts/create', 'PostController@store')->name('posts.store');
-
     // Chuyển đổi ngôn ngữ cho website
     Route::get('change-language/{language}', 'LanguageController@changeLanguage')->name('user.change-language');
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Auth::routes();
+
+    Route::group(["middleware" => "auth"], function () {
+
+        Route::get('/home', 'HomeController@index')->name('home');
+
+        // Hiển thị giao diện thêm mới bài viết
+        Route::get('/posts/create', 'PostController@create')->name('posts.create');
+
+        // Tạo mới bài viết
+        Route::post('/posts/create', 'PostController@store')->name('posts.store');
+    });
 });
+
+
